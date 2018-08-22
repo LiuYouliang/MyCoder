48ca48cf939e66b191aea1671d89a9e30db15d3b
LiuYouliang <34112840+LiuYouliang@users.noreply.github.com>
2018-08-22 14:55:58 +0800

    上传文件
    上传了一个py文件

---
+++ config.py
@@ -0,0 +1,13 @@
+
+
+#一个简单的配置文件
+
+[numbers]
+
+pi: 3.141592653589731
+
+[messages]
+
+greeting: Welcom to the area calculation program!
+question: Please enter the radius:
+result_message: The ares is
